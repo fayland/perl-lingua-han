2b57a68dd9e8fa8e7eef73c81a8a2ec6275a325b
Fayland Lam <fayland@gmail.com>
2010-06-07 09:58:36 +0800

    bump up to ver

--- Lingua-Han-Cantonese/lib/Lingua/Han/Cantonese.pm
+++ Lingua-Han-Cantonese/lib/Lingua/Han/Cantonese.pm
@@ -3,7 +3,7 @@ package Lingua::Han::Cantonese;
 use warnings;
 use strict;
 use vars qw($VERSION);
-$VERSION = '0.06';
+$VERSION = '0.07';
 
 use File::Spec;
 use Lingua::Han::Utils qw/Unihan_value/;
--- Lingua-Han-PinYin/lib/Lingua/Han/PinYin.pm
+++ Lingua-Han-PinYin/lib/Lingua/Han/PinYin.pm
@@ -2,7 +2,7 @@ package Lingua::Han::PinYin;
 
 use strict;
 use warnings;
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 use File::Spec ();
 use Lingua::Han::Utils qw/Unihan_value/;
